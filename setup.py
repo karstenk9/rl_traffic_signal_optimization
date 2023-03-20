9e57aa141bd6c649ae88af4cfe43ebcde5a440c0
Jennifer Hahn <hahn.jennifer@live.de>
2023-03-20 11:55:03 +0100

    initialize repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The project investigates traffic polciies, and more specifically intelligent traffic signal control, based on reinforcement learning to identify measures that can reduce pollutant emissions within cities. SUMO (Simulation of Urban Mobility) is used for a microscopic traffic simulation and analysis within the inner city of Mannheim.',
+    author='Jennifer Hahn',
+    license='MIT',
+)
