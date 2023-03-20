c2d20308f64f75ae9929f8fadf9fa77ff26a0e08
Jennifer Hahn <hahn.jennifer@live.de>
2023-03-20 16:04:36 +0100

    sumo traci

--- src/features/access_sumo.py
+++ src/features/access_sumo.py
@@ -3,10 +3,30 @@ import sys
 
 sys.path.append(os.path.join('c:', os.sep, '/opt/homebrew/opt/sumo/share/sumo/tools'))
 
-import traci
-print("LOADPATH:", '\n'.join(sys.path))
-print("TRACIPATH:", traci.__file__)
-sys.exit()
+sumoBinary = "sumo-gui"
+sumoCmd = [sumoBinary, "-c", "/Users/jenniferhahn/Library/CloudStorage/OneDrive-UniversitätMannheim/01_Master_Thesis/urban_mobility_simulation/models/Mannheim145_export06032023/osm.sumocfg"]
 
-sumoBinary = 'Applications/sumo-gui'
-traci.start([sumoBinary,'-c','run.sumocfg','--save-configuration','debug.sumocfg'])
+
+import traci
+
+# start the TraCI server
+traci.start(sumoCmd)
+
+# run the simulation for 1000 time steps
+for i in range(1000):
+    # get the current simulation time
+    #current_time = traci.simulation.getCurrentTime()
+
+    # get the list of vehicles on the road network
+    vehicle_list = traci.vehicle.getIDList()
+
+    # print the number of vehicles on the road network
+    #print("Current time: ", current_time)
+    print("Number of vehicles: ", len(vehicle_list))
+
+    # advance the simulation by one step
+    traci.simulationStep()
+
+# stop the TraCI server
+traci.close()
+
