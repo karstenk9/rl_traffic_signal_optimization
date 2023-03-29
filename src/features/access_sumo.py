b0cccbbea1eb85986ba98b99863f47776220a205
Jennifer Hahn <hahn.jennifer@live.de>
2023-03-29 17:40:16 +0200

    try sumo output

--- src/features/access_sumo.py
+++ src/features/access_sumo.py
@@ -1,13 +1,17 @@
 import os
 import sys
+from sumolib import checkBinary
+import traci
 
-sys.path.append(os.path.join('c:', os.sep, '/opt/homebrew/opt/sumo/share/sumo/tools'))
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
 
 sumoBinary = "sumo-gui"
 sumoCmd = [sumoBinary, "-c", "/Users/jenniferhahn/Library/CloudStorage/OneDrive-UniversitätMannheim/01_Master_Thesis/urban_mobility_simulation/models/Mannheim145_export06032023/osm.sumocfg"]
 
-
-import traci
 
 # start the TraCI server
 traci.start(sumoCmd)
@@ -15,13 +19,13 @@ traci.start(sumoCmd)
 # run the simulation for 1000 time steps
 for i in range(1000):
     # get the current simulation time
-    #current_time = traci.simulation.getCurrentTime()
+    current_time = traci.simulation.getCurrentTime()
 
     # get the list of vehicles on the road network
     vehicle_list = traci.vehicle.getIDList()
 
     # print the number of vehicles on the road network
-    #print("Current time: ", current_time)
+    print("Current time: ", current_time)
     print("Number of vehicles: ", len(vehicle_list))
 
     # advance the simulation by one step
---
+++ src/features/traci_get_output.py
@@ -0,0 +1,86 @@
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+import sys
+import optparse
+import random
+
+# we need to import python modules from the $SUMO_HOME/tools directory
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+from sumolib import checkBinary  # noqa
+import traci  # noqa
+
+def run():
+
+    # run the simulation for 1000 time steps
+    for i in range(1000):
+        # get the current simulation time
+        current_time = traci.simulation.getCurrentTime()
+
+        # get the list of vehicles on the road network
+        vehicle_list = traci.vehicle.getIDList()
+
+        # print the number of vehicles on the road network
+        print("Current time: ", current_time)
+        print("Number of vehicles: ", len(vehicle_list))
+
+        # advance the simulation by one step
+        traci.simulationStep()
+
+    # stop the TraCI server
+    traci.close()
+
+
+    """execute the TraCI control loop"""
+    step = 0
+    # we start with phase 2 where EW has green
+    traci.trafficlight.setPhase("0", 2)
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
+        if traci.trafficlight.getPhase("0") == 2:
+            # we are not already switching
+            if traci.inductionloop.getLastStepVehicleNumber("0") > 0:
+                # there is a vehicle from the north, switch
+                traci.trafficlight.setPhase("0", 3)
+            else:
+                # otherwise try to keep green for EW
+                traci.trafficlight.setPhase("0", 2)
+        step += 1
+    traci.close()
+    sys.stdout.flush()
+
+
+def get_options():
+    optParser = optparse.OptionParser()
+    optParser.add_option("--nogui", action="store_true",
+                         default=False, help="run the commandline version of sumo")
+    options, args = optParser.parse_args()
+    return options
+
+
+# this is the main entry point of this script
+if __name__ == "__main__":
+    options = get_options()
+
+    # this script has been called from the command line. It will start sumo as a
+    # server, then connect and run
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+
+    # first, generate the route file for this simulation
+    #generate_routefile()
+
+    # this is the normal way of using traci. sumo is started as a
+    # subprocess and then the python script connects and runs
+    traci.start([sumoBinary, "-c", "../../models/Mannheim145_export06032023/osm.sumocfg",
+                             "--tripinfo-output", "tripinfo.xml"])
+    run()
