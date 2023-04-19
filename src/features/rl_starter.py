cedf7e2b320570ab947fd45f9b386c5d665c7427
Jennifer Hahn <hahn.jennifer@live.de>
2023-04-19 12:04:28 +0200

    build simple RL test version

--- src/features/rl_starter.py
+++ src/features/rl_starter.py
@@ -1,2 +1,170 @@
+
+from __future__ import absolute_import
+from __future__ import print_function
+
 import os
 import sys
+import optparse
+import random
+import numpy as np0
+
+from sumolib import checkBinary
+import traci 
+
+# import python modules from the $SUMO_HOME/tools directory
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+def rl_env():
+    
+    """
+    Define reinforcement learning environment
+    with states, actions, and rewards
+    """
+
+    n_actions = 2  # number of possible actions: stop or go
+    n_states = 4  # number of possible states: North-South and East-West traffic light color
+    Q = np.zeros([n_states, n_actions])  # Q-table for storing state-action values
+    gamma = 0.8  # discount factor for future rewards
+    alpha = 0.5  # learning rate for updating Q-values
+    epsilon = 0.1  # exploration rate for choosing random actions
+
+
+def run():
+    
+    """
+    Function to run the simulation for 1000 time steps
+    and print the number of vehicles on the road network
+    """
+
+    # run the simulation for 1000 time steps
+    for i in range(1000):
+        # get the current simulation time
+        current_time = traci.simulation.getCurrentTime()
+
+        # get the list of vehicles on the road network
+        vehicle_list = traci.vehicle.getIDList()
+
+        # print the number of vehicles on the road network
+        print("Current time: ", current_time)
+        print("Number of vehicles: ", len(vehicle_list))
+
+        # advance the simulation by one step
+        traci.simulationStep()
+
+    # stop the TraCI server
+    traci.close()
+
+
+    #"""execute the TraCI control loop"""
+    #step = 0
+    # we start with phase 2 where EW has green
+    #traci.trafficlight.setPhase("0", 2)
+    #while traci.simulation.getMinExpectedNumber() > 0:
+    #    traci.simulationStep()
+    #    if traci.trafficlight.getPhase("0") == 2:
+    #        # we are not already switching
+    #        if traci.inductionloop.getLastStepVehicleNumber("0") > 0:
+    #            # there is a vehicle from the north, switch
+    #            traci.trafficlight.setPhase("0", 3)
+    #        else:
+    #           # otherwise try to keep green for EW
+    #            traci.trafficlight.setPhase("0", 2)
+    #    step += 1
+    #traci.close()
+    #sys.stdout.flush()
+
+
+def get_options():
+    optParser = optparse.OptionParser()
+    optParser.add_option("--nogui", action="store_true",
+                         default=False, help="run the commandline version of sumo")
+    options, args = optParser.parse_args()
+    return options
+
+
+# this is the main entry point of this script
+if __name__ == "__main__":
+    options = get_options()
+
+    # this script has been called from the command line. It will start sumo as a
+    # server, then connect and run
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+
+    # first, generate the route file for this simulation
+    #generate_routefile()
+
+    # this is the normal way of using traci. sumo is started as a
+    # subprocess and then the python script connects and runs
+    traci.start([sumoBinary, "-c", "urban_mobility_simulation/models/Mannheim145_export06032023/osm.sumocfg",
+                                "--tripinfo-output", "urban_mobility_simulation/src/data/tripinfo.xml",
+                                "--output-prefix", "TIME",
+                                "--device.emissions.probability", "1.0",
+                                "--emission-output", "urban_mobility_simulation/src/data/emission_info.xml"])
+
+    run()
+
+
+
+import traci
+import numpy as np
+import random
+
+# create SUMO simulation environment
+sumoBinary = "/usr/bin/sumo-gui"  # path to SUMO binary
+sumoCmd = [sumoBinary, "-c", "crossing.sumocfg"]  # path to SUMO configuration file
+traci.start(sumoCmd)
+
+# define reinforcement learning environment
+n_actions = 2  # number of possible actions: stop or go
+n_states = 4  # number of possible states: North-South and East-West traffic light color
+Q = np.zeros([n_states, n_actions])  # Q-table for storing state-action values
+gamma = 0.8  # discount factor for future rewards
+alpha = 0.5  # learning rate for updating Q-values
+epsilon = 0.1  # exploration rate for choosing random actions
+
+# define simulation loop
+for step in range(1000):
+    traci.simulationStep()  # advance the simulation by one step
+    
+    # get current state
+    north_south_color = traci.trafficlight.getRedYellowGreenState("n-s")
+    east_west_color = traci.trafficlight.getRedYellowGreenState("e-w")
+    if north_south_color == "G" and east_west_color == "r":
+        state = 0  # North-South green, East-West red
+    elif north_south_color == "r" and east_west_color == "G":
+        state = 1  # North-South red, East-West green
+    elif north_south_color == "y" and east_west_color == "r":
+        state = 2  # North-South yellow, East-West red
+    elif north_south_color == "r" and east_west_color == "y":
+        state = 3  # North-South red, East-West yellow
+    
+    # choose action using epsilon-greedy strategy
+    if random.uniform(0, 1) < epsilon:
+        action = random.randint(0, n_actions-1)  # choose random action
+    else:
+        action = np.argmax(Q[state])  # choose best action based on current Q-values
+        
+    # take action and get reward
+    if action == 0:
+        traci.trafficlight.setRedYellowGreenState("n-s", "rrrGrrr")  # allow North-South traffic to move
+        traci.trafficlight.setRedYellowGreenState("e-w", "rrrRrrr")  # stop East-West traffic
+        reward = -1  # negative reward for stopping traffic
+    else:
+        traci.trafficlight.setRedYellowGreenState("n-s", "rrrRrrr")  # stop North-South traffic
+        traci.trafficlight.setRedYellowGreenState("e-w", "rrrGrrr")  # allow East-West traffic to move
+        reward = -1  # negative reward for stopping traffic
+    
+    # get new state and update Q-values
+    north_south_color = traci.trafficlight.getRedYellowGreenState("n-s")
+    east_west_color = traci.trafficlight.getRedYellowGreenState("e-w")
+    if north_south_color == "G" and east_west_color == "r":
+        new_state = 0  # North-South green, East-West red
+    elif north_south_color == "r" and east_west_color == "G":
+        new_state = 1
