eac49c32e620798b14c2961d863b6271764ea9aa
JenniferHahn <45081580+JenniferHahn@users.noreply.github.com>
2023-07-13 14:27:04 +0200

    update import structure

--- src/experiments/ppo_server_test.py
+++ src/experiments/ppo_server_test.py
@@ -5,26 +5,17 @@ import gym
 import numpy as np
 
 import platform
-# on the server (platform = Linux) we use libsumo and also don't need the tools in the path
-# if platform.system() != "Linux":
-#     if 'SUMO_HOME' in os.environ:
-#         tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
-#         sys.path.append(tools)  # we need to import python modules from the $SUMO_HOME/tools directory
-#     else:
-#         sys.exit("please declare environment variable 'SUMO_HOME'")
-#     import traci
-# else:
 import libsumo as traci
 
 from sumolib import checkBinary
 
 from stable_baselines3.ppo.ppo import PPO
-
-from sumo_rl import SumoEnvironment
-
+from environment.test_env import SumoEnvironment
 
 env = SumoEnvironment(
-    net_file="urban_mobility_simulation/models/20230502_SUMO_MA/osm.net.xml",
+    net_file="urban_mobility_simulation/models/20230502_SUMO_MA/osm.net.xml, \
+            urban_mobility_simulation/models/20230502_SUMO_MA/pt/stops.add.xml, \
+            urban_mobility_simulation/models/20230502_SUMO_MA/osm.poly.xml",
     single_agent=True,
     route_file="urban_mobility_simulation/models/20230502_SUMO_MA/routes.xml, \
                 urban_mobility_simulation/models/20230502_SUMO_MA/osm.bicycle.trips.xml,\
@@ -32,7 +23,7 @@ env = SumoEnvironment(
                 urban_mobility_simulation/models/20230502_SUMO_MA/osm.truck.trips.xml, \
                 urban_mobility_simulation/models/20230502_SUMO_MA/pt/ptflows.rou.xml, \
                 urban_mobility_simulation/models/20230502_SUMO_MA/osm.passenger.trips.xml", 
-    out_csv_name="urban_mobility_simulation/src/data/model_outputs/ppo_woPT",
+    out_csv_name="urban_mobility_simulation/src/data/model_outputs/ppo_withPT",
     use_gui=False,
     num_seconds=5000,
     yellow_time=4,
