32c9b13981cc97055f67cf40ee2e2929cc7de7a4
JenniferHahn <hahn.jennifer@live.de>
2023-09-02 10:21:41 +0200

    further analysis

--- src/experiments/ppo_train.py
+++ src/experiments/ppo_train.py
@@ -20,11 +20,11 @@ env = custom_env.MA_grid_train(use_gui=False,
                             traffic_lights= ['tls_159','tls_160', 'tls_161'], 
                             sumo_warnings=False,
                             begin_time=25200,
-                            num_seconds=4500, # sim_max_time = begin_time + num_seconds
-                            out_csv_name='/Users/jenniferhahn/Documents/GitHub/urban_mobility_simulation/src/data/model_outputs/diff-waiting-time_200000',
-                            additional_sumo_cmd="--emission-output /Users/jenniferhahn/Documents/GitHub/urban_mobility_simulation/src/data/model_outputs/emission_diff-waiting-time.xml, \
-                                                --lanedata-output /Users/jenniferhahn/Documents/GitHub/urban_mobility_simulation/src/data/model_outputs/lane_diff-waiting-time.xml",
-                            )
+                            num_seconds=4500,) # sim_max_time = begin_time + num_seconds
+                            # out_csv_name='/Users/jenniferhahn/Documents/GitHub/urban_mobility_simulation/src/data/model_outputs/diff-waiting-time_200000',
+                            # additional_sumo_cmd="--emission-output /Users/jenniferhahn/Documents/GitHub/urban_mobility_simulation/src/data/model_outputs/emission_diff-waiting-time.xml, \
+                            #                     --lanedata-output /Users/jenniferhahn/Documents/GitHub/urban_mobility_simulation/src/data/model_outputs/lane_diff-waiting-time.xml",
+                            # )
 max_time = env.unwrapped.env.sim_max_time
 delta_time = env.unwrapped.env.delta_time
 
@@ -56,7 +56,7 @@ model = PPO(
     n_epochs=10,
     clip_range=0.3,
     batch_size=64,
-    tensorboard_log="./logs/MA_grid/avg_brake_NEW",
+    tensorboard_log="./logs/MA_grid/waitingTime",
     device='auto' # use 'auto' for cpu & mps for GPU
 )
 
@@ -64,7 +64,7 @@ model = PPO(
 print("Starting training")
 model.learn(total_timesteps=200000)
 
-model.save('urban_mobility_simulation/src/data/logs/waitingTime_900')
+model.save('urban_mobility_simulation/src/data/logs/waitingTime_200')
 
 print("Training finished. Starting evaluation")
 mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=1)
