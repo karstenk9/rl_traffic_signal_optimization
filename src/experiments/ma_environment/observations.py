085bb9871a3f4b5b04c13ebbab52e2642f1d74f7
karstenk9 <karstenkarres@gmail.com>
2024-03-11 23:38:32 +0300

    Comment out new observation

--- src/experiments/ma_environment/observations.py
+++ src/experiments/ma_environment/observations.py
@@ -48,13 +48,13 @@ class DefaultObservationFunction(ObservationFunction):
         min_green = [0 if self.ts.time_since_last_phase_change < self.ts.min_green + self.ts.yellow_time else 1]
         density = self.ts.get_lanes_density()
         queue = self.ts.get_lanes_queue()
-        paths = [self.ts.get_paths_through_tls()]
+        paths = [] #[self.ts.get_paths_through_tls()]
         observation = np.array(phase_id + min_green + density + queue + paths, dtype=np.float32)
         return observation
 
     def observation_space(self) -> spaces.Box:
         """Return the observation space."""
         return spaces.Box(
-            low=np.zeros(self.ts.num_green_phases + 1 + 2 * len(self.ts.lanes) +1, dtype=np.float32),
-            high=np.ones(self.ts.num_green_phases + 1 + 2 * len(self.ts.lanes) +1, dtype=np.float32),
+            low=np.zeros(self.ts.num_green_phases + 1 + 2 * len(self.ts.lanes), dtype=np.float32), # +1
+            high=np.ones(self.ts.num_green_phases + 1 + 2 * len(self.ts.lanes), dtype=np.float32), # +1
         )
